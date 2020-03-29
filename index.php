e255b6b08ae9181cfce513cf8ce39ffc6dcb739b
Andreas Harryanto <62596434+andreasharryanto@users.noreply.github.com>
2020-03-29 14:39:56 +0700

    add select
    add view data from database

--- index.php
+++ index.php
@@ -6,6 +6,25 @@ $conn = mysqli_connect('localhost', 'root', '', 'university');
 if(!$conn) die('error : ' . mysqli_connect_error());
 else echo 'success connect db<br>';
 
+$result = mysqli_query($conn, "SELECT * FROM student");
+if( !$result ){
+    echo mysqli_error($conn);
+}
+
+//you can use view-source:http://localhost/php_database/
+
+//mysqli_fetch_row() index by number = var_dump($students[3]);
+//mysqli_fetch_assoc() index by string (associative array) = var_dump($students["studentaddress"]);
+//mysqli_fetch_array() index by number or string
+//mysqli_fetch_object() return object = var_dump($students->studentaddress);
+
+// $students = mysqli_fetch_assoc($result);
+// var_dump($students->studentaddress);
+
+// while($students = mysqli_fetch_assoc($result)){
+//     var_dump($students[3]);
+// }
+
 //DRY
 function insert_data($conn, $studentname, $studentage, $studentaddress){
     $query = "INSERT INTO student(studentname, studentage, studentaddress)
@@ -27,6 +46,7 @@ mysqli_close($conn);
 <!DOCTYPE html>
 <head>
     <title>University Website</title>
+    <script type="text/javascript" src="index.js"></script>
 </head>
 <body>
     <form action="" method="POST">
@@ -35,5 +55,21 @@ mysqli_close($conn);
         <br><label for="address">Address : </label><input type="text" name="address" id="address">
         <br><button type="submit" name="submit">Add</button>
     </form>
+    <table>
+        <tr>
+            <th>Student ID</th>
+            <th>Student Name</th>
+            <th>Student Age</th>
+            <th>Studnet Address</th>
+        </tr>
+        <?php while($students = mysqli_fetch_assoc($result)) : ?>
+        <tr>
+            <td><?= $students["studentid"]?></td>
+            <td><?= $students["studentname"]?></td>
+            <td><?= $students["studentage"]?></td>
+            <td><?= $students["studentaddress"]?></td>
+        </tr>
+        <?php endwhile;?>
+    </table>
 </body>
 </html>
